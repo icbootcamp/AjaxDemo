c723292d6f7b808c809dd273604bea69d2c777e8
craigrabbitt <craigrabbitt@VIRTUALWIN10>
2017-05-01 14:34:33 +1200

    opps forgot this

--- AjaxDemo/Controllers/HomeController.cs
+++ AjaxDemo/Controllers/HomeController.cs
@@ -3,11 +3,14 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using AjaxDemo.Models;
 
 namespace AjaxDemo.Controllers
 {
     public class HomeController : Controller
     {
+        SchoolEntities db = new SchoolEntities();
+
         public ActionResult Index()
         {
             return View();
@@ -25,6 +28,30 @@ namespace AjaxDemo.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult GetCourseList()
+        {
+            var model = db.Courses.ToList().
+                Select(x => new CourseViewModel { Id = x.CourseID, Title = x.Title }).
+                ToList();
+            return View(model);
+        }
+
+        public ActionResult GetCourseById(int id)
+        {
+            var getCourse = db.Courses.Find(id);
+            CourseViewModel model = new CourseViewModel()
+            {
+                Id = getCourse.CourseID,
+                Title = getCourse.Title
+            };
+            return Json(model, JsonRequestBehavior.AllowGet);
+        }
+
+        public ActionResult SaveData(CourseViewModel model)
+        {
+            return Json(new { success = true });
         }
     }
 }
---
+++ AjaxDemo/Models/CourseViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AjaxDemo.Models
+{
+    public class CourseViewModel
+    {
+        public string Title { get; set; }
+        public int Id { get; set; }
+        
+    }
+}
